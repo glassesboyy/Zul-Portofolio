d4a2e1e41814b202cbcb22f9be828539ec74e0fd
glassesboyy <aliperwira26@gmail.com>
2025-03-14 02:06:15 +0700

    menambahkan prop key unik di komponen timeline agar tidak lagi muncul pesan error pada terminal saat server dijalankan

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -66,7 +66,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         {/* Timeline Content */}
         <div ref={ref} className="relative max-w-7xl mx-auto">
           {data.map((item, index) => (
-            <div className="flex justify-start pt-10 md:pt-40 md:gap-10">
+            <div
+              key={`timeline-${index}`}
+              className="flex justify-start pt-10 md:pt-40 md:gap-10"
+            >
               {/* Timeline Point and Year */}
               <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
                 <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black flex items-center justify-center border border-violet-500/20">
